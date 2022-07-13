b7d6f104ba6bebaf02e0f50ac2e2814071d28d39
DoosicEE <strou73@naver.com>
2022-07-14 05:05:26 +0900

    Initial commit

---
+++ src/test/java/com/functionaljava/functionaljava/FunctionalJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.functionaljava.functionaljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
