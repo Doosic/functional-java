0b1b43983c09cfd5c26b27c787807f89f796e81c
DoosicEE <strou73@naver.com>
2022-07-17 06:52:54 +0900

    Add #8 Method Reference 2
    도메인 즉, Entity 를 람다로 꺼내오는 방법을 배웠다.

--- src/main/java/com/functionaljava/functionaljava/chapter5/Chapter5Section2.java
+++ src/main/java/com/functionaljava/functionaljava/chapter5/Chapter5Section2.java
@@ -1,5 +1,9 @@
 package com.functionaljava.functionaljava.chapter5;
 
+import com.functionaljava.functionaljava.chapter5.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
 
@@ -13,5 +17,18 @@ public class Chapter5Section2 {
         BiPredicate<String, String> strEquals = String::equals;
         boolean test = strEquals.test("hello", "Not Hello");
         System.out.println(test);
+
+        List<User> users = new ArrayList<>();
+        users.add(new User(3, "Alice"));
+        users.add(new User(7, "Charlie"));
+        users.add(new User(4, "Bob"));
+
+        printUserField(users, User::getId);
+    }
+
+    public static void printUserField(List<User> users, Function<User, Object> getter){
+        for (User user : users) {
+            System.out.println(user);
+        }
     }
 }
