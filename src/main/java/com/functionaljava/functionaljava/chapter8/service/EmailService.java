9b3d3239e4e486f3f4d132f0b610319589897c4e
DoosicEE <strou73@naver.com>
2022-07-27 22:41:47 +0900

    Add #16 Partitioning By
    그룹을 묶어서 결과를 내지만, 그룹의 key 값이 true 와 false 만 존재한다. 어떠한 조건의 결과가 true 인 그룹, false 인 그룹.

---
+++ src/main/java/com/functionaljava/functionaljava/chapter8/service/EmailService.java
@@ -0,0 +1,15 @@
+package com.functionaljava.functionaljava.chapter8.service;
+
+import com.functionaljava.functionaljava.chapter8.model.User;
+
+public class EmailService {
+    public void sendPlayWithFriendsEmail(User user){
+        user.getEmailAddress().ifPresent(email ->
+                System.out.println("Sending 'Play With Friends' email to " + email));
+    }
+
+    public void sendMakeMoreFriendsEmail(User user){
+        user.getEmailAddress().ifPresent(email ->
+                System.out.println("Sending 'Make More Friends' email to " + email));
+    }
+}
